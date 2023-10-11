25ae477a3443b299873ee802591ba026bda9f966
akshaypagar232 <akshaypagar232@gmai.com>
2023-10-11 12:52:27 +0530

    .

--- src/main/java/com/payment/controller/UserController.java
+++ src/main/java/com/payment/controller/UserController.java
@@ -1,7 +1,10 @@
 package com.payment.controller;
 
+import java.util.Map;
+
 import javax.validation.Valid;
 
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,12 +14,17 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.payment.config.PageableResponse;
 import com.payment.dto.UserDto;
 import com.payment.service.UserService;
+import com.razorpay.Order;
+import com.razorpay.RazorpayClient;
+import com.razorpay.RazorpayException;
 
+import lombok.experimental.var;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -26,8 +34,43 @@ public class UserController {
 
 	@Autowired
 	private UserService userService;
+	
+	@PostMapping("/create")
+	@ResponseBody
+	public ResponseEntity<String> createOrder(@RequestBody Map<String, UserDto> data) {
 
-	@PostMapping()
+		log.info("Initiated request pass service for save the Payment details");
+
+		Integer.parseInt(data.get("amount").toString());
+
+		RazorpayClient client = null;
+		
+		try {
+			client = new RazorpayClient("jhdgfe", "ghhvj");
+		} catch (RazorpayException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		JSONObject js = new JSONObject();
+		js.put("amount", 5000);
+		js.put("currency", "INR");
+		js.put("receipt", "txn_123");
+		
+		try {
+			
+			Order order = client.Orders.create(js);
+		} catch (RazorpayException e) {
+
+			e.printStackTrace();
+		}
+		
+		log.info("Completed request for save the Payment details");
+
+		return new ResponseEntity<String>("Success", HttpStatus.CREATED);
+	}
+
+	@PostMapping
 	public ResponseEntity<UserDto> createPayment(@Valid @RequestBody UserDto userDto, @RequestParam String cartId) {
 
 		log.info("Initiated request pass service for save the Payment details");
